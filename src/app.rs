6f99c6be689b2fac8dd1ec86fe8d1ada542d7c53
Andrew Peterson <apeterson2775@gmail.com>
2023-11-13 09:48:25 -0600

    nexus init

---
+++ src/app.rs
@@ -0,0 +1,196 @@
+use crate::error_template::{AppError, ErrorTemplate};
+use leptos::*;
+use leptos_meta::*;
+use leptos_router::*;
+
+#[component]
+pub fn App() -> impl IntoView {
+    // Provides context that manages stylesheets, titles, meta tags, etc.
+    provide_meta_context();
+    view! {
+        <Stylesheet id="leptos" href="/pkg/nexus.css"/>
+
+        // sets the document title
+        <Title text="Welcome to Leptos"/>
+
+        <link rel="preconnect" href="https://fonts.googleapis.com"/>
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+        <link
+            href="https://fonts.googleapis.com/css2?family=Roboto+Slab&display=swap"
+            rel="stylesheet"
+        />
+
+        // content for this welcome page
+        <Router fallback=|| {
+            let mut outside_errors = Errors::default();
+            outside_errors.insert_with_default_key(AppError::NotFound);
+            view! { <ErrorTemplate outside_errors/> }.into_view()
+        }>
+            <body>
+                <Header/>
+                <main>
+                    <Routes>
+                        <Route path="" view=HomePage/>
+                        <Route path="about" view=AboutPage/>
+                        <Route path="community" view=CommunityPage/>
+                        <Route path="terms_and_conditions" view=TermsAndConditionsPage/>
+                        <Route path="credits" view=CreditsPage/>
+                        <Route path="support" view=SupportFAQPage/>
+                        <Route path="log_in" view=LoginPage/>
+                    </Routes>
+                </main>
+                <Footer/>
+            </body>
+        </Router>
+    }
+}
+
+/// Renders the home page of your application.
+#[component]
+fn HomePage() -> impl IntoView {
+    // Creates a reactive value to update the button
+    let (count, set_count) = create_signal(0);
+    let on_click = move |_| set_count.update(|count| *count += 1);
+
+    view! {
+        <h1>"Welcome to Leptos!"</h1>
+        <button on:click=on_click class:red=move || count() % 2 == 1>
+            "Click Me: "
+            {count}
+        </button>
+    }
+}
+
+#[component]
+fn AboutPage() -> impl IntoView {
+    view! {
+        <h1>"$GAME_NAME is an exploration into the cave world of $WORLD_NAME."</h1>
+        <p>Build, craft, explore, and conquer the ancient ruined cities beneath the earth.</p>
+    }
+}
+
+#[component]
+fn CommunityPage() -> impl IntoView {
+    view! { <A href="https://discord.gg/EuqSvxDPRY">Join the official Discord!</A> }
+}
+
+#[component]
+fn TermsAndConditionsPage() -> impl IntoView {
+    view! {
+        <p>This is where the TOS and EULA will go once they are created.</p>
+        <p>
+            In the meantime, understand I do not take responsibility for any harms that may come to the player while playing.
+        </p>
+        <p>You are allowed to stream this game</p>
+    }
+}
+
+#[component]
+fn CreditsPage() -> impl IntoView {
+    view! {}
+}
+
+#[component]
+fn SupportFAQPage() -> impl IntoView {
+    view! {
+        <h1>FAQ</h1>
+        <ul>
+            <li>What</li>
+        </ul>
+        <p>Contact andrewpetersongamedev@gmail.com for any issues with support</p>
+    }
+}
+
+#[component]
+fn LoginPage() -> impl IntoView {
+    // TODO: check security on using signals to store password
+    let (username, set_username) = create_signal("Username".to_string());
+    let (password, set_password) = create_signal("Password".to_string());
+    let (email, set_email) = create_signal("Email".to_string());
+    view! {
+        <div class="form-container">
+            <form id="sign-in-form">
+                <label for="username">Username:</label>
+                <br/>
+                <input
+                    type="text"
+                    on:input=move |ev| {
+                        set_username(event_target_value(&ev));
+                    }
+
+                    prop:value=username
+                />
+                <br/>
+                <label for="email">Email:</label>
+                <br/>
+                <input
+                    type="text"
+                    on:input=move |ev| {
+                        set_email(event_target_value(&ev));
+                    }
+
+                    prop:value=email
+                />
+
+                <br/>
+                <label for="password">Password:</label>
+                <br/>
+                <input
+                    type="password"
+                    on:input=move |ev| {
+                        set_password(event_target_value(&ev));
+                    }
+
+                    prop:value=password
+                />
+                <br/>
+                <input type="submit"/>
+
+            </form>
+            <form id="register-form"></form>
+        </div>
+
+        <br/>
+
+        <div>
+            Forgotten your <a href="recover">username</a> or <a href="recover">password?</a>
+            You can recover them using your email address at this <a href="recover">link.</a>
+        </div>
+    }
+}
+
+#[component]
+fn Header() -> impl IntoView {
+    view! {
+        <header class="header">
+            <div class="headerlinks">
+                <img/>
+                <nav class="nav">
+                    <A href="">Home</A>
+                    <A href="about">About</A>
+                    <A href="community">Community</A>
+                    <A href="support">Help</A>
+                </nav>
+                <div class="authgroup">
+                    <A href="log_in">Log in</A>
+                    |
+                    <A href="log_in">Register</A>
+                </div>
+            </div>
+        </header>
+    }
+}
+
+#[component]
+fn Footer() -> impl IntoView {
+    view! {
+        <footer class:footer=true>
+            <div class="footeritems">
+                Copyright 2023-2023 Andrew Peterson.
+                <A href="terms_and_conditions">Terms and conditions</A>
+                <A href="credits">Credits</A> <A href="support">Support / FAQ</A>
+            </div>
+        </footer>
+    }
+}
+
